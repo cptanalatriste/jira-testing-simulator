a12fa86adfba626a358726a99436cca36961e5ab
Carlos G. Gavidia <cgavidiac@gmail.com>
2016-02-19 20:25:24 +0000

    Initial version of the simulation

--- game_simulation.py
+++ game_simulation.py
@@ -1,17 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Feb 13 18:11:45 2016
-
-@author: Carlos G. Gavidia
-"""
-
+""" Support for the multi-agent simulation """
 import random
-import data_analysis
 
 from scipy import stats
+import matplotlib.pyplot as plt
+import data_analysis
 
-TIME_FRAMES = 3
-NUMBER_OF_TESTERS = 2
+TIME_FRAMES = 24
+NUMBER_OF_TESTERS = 9
 
 SEVERE_KEY = 'severe'
 DEFAULT_KEY = 'default'
@@ -21,21 +16,31 @@ REPORTED_SUFFIX = '_reported'
 FIXED_SUFFIX = '_fixed'
 INFLATED_SUFFIX = '_inflated'
 
-class DeveloperTeam:
+SEVERE_VALUE = 7
+DEFAULT_VALUE = 3
+NONSEVERE_VALUE = 1
+
+TESTER_NAMES = ['sangeethah', 'rayeesn', 'chandanp', 'sailaja', 'alena1108',
+                'jessicawang', 'minchen07', 'parth', 'nitinme']
+POPULATION_STRATEGIES = [(0.08, 0.89), (0.06, 0.97), (0.1, 0.84), (0.06, 0.34),
+                         (0.1, 0.5), (0, 0), (0.55, 0), (0.086, 0.56), (0.1, 0)]
+
+
+class DeveloperTeam(object):
+    """ A Developer Team that fixes defects based on reporter priorities """
+
     def __init__(self, kernel):
         self.kernel = kernel
 
     def fix(self, tester_reports):
         """ Giving a list of reports, it returns how many of them got fixed """
-        #TODO(cgavidia): We might need a report class.
-        #TODO(cgavidia): This is a dummy implementation. We need to consider
-        # the priorities and the stochastic dev team productivity.
+
         print 'tester_reports ', tester_reports
         productivity = self.get_developer_productivity(tester_reports)
         fix_reports = [{SEVERE_KEY: 0,
                         NON_SEVERE_KEY: 0,
                         DEFAULT_KEY: 0}
-                        for _ in range(len(tester_reports))]
+                       for _ in range(len(tester_reports))]
 
         severe_reports = []
         default_reports = []
@@ -48,19 +53,21 @@ class DeveloperTeam:
             non_severe_reports.extend([tester_index for _ in range(report[NON_SEVERE_KEY])])
 
         severe_fixed = self.process_priority_batch(productivity, severe_reports, fix_reports,
-                                    SEVERE_KEY)
+                                                   SEVERE_KEY)
         default_fixed = 0
 
         if productivity - severe_fixed > 0:
             default_fixed = self.process_priority_batch(productivity - severe_fixed,
-                                                       default_reports, fix_reports,
-                                                       DEFAULT_KEY)
+                                                        default_reports, fix_reports,
+                                                        DEFAULT_KEY)
 
         if productivity - severe_fixed - default_fixed > 0:
             self.process_priority_batch(productivity - severe_fixed - default_fixed,
-                                                       non_severe_reports, fix_reports,
-                                                       NON_SEVERE_KEY)
+                                        non_severe_reports, fix_reports,
+                                        NON_SEVERE_KEY)
 
+        # TODO(cgavidia): Big assumption! The severe reports that didn't get to fixed
+        # are ignored so far. We not considerer the possibility to be fixed later.
         print 'fix_reports ', fix_reports
         return fix_reports
 
@@ -99,12 +106,12 @@ class DeveloperTeam:
 
         return fixes_delivered
 
-class StochasticInflationStrategy:
+class StochasticInflationStrategy(object):
     """ Defines a particular strategy for testing on a release """
 
-    def __init__(self, for_default = 0, for_nonsevere = 0):
-        self.for_nonsevere = for_nonsevere
-        self.for_default = for_default
+    def __init__(self, inflation_prob=(0, 0)):
+        self.for_default = inflation_prob[0]
+        self.for_nonsevere = inflation_prob[1]
 
     def report(self, issues_found):
         """ Makes a report, with the inflation results based on probabilities.
@@ -132,6 +139,8 @@ class StochasticInflationStrategy:
         return inflation, report
 
     def inflate(self, issues, inflation_prob):
+        """ Return the number of issues to inflate based on the probability """
+
         inflated_issues = 0
 
         for _ in range(issues):
@@ -140,33 +149,32 @@ class StochasticInflationStrategy:
 
         return inflated_issues
 
-class Tester:
+class Tester(object):
     """ A Tester, that reports defect for a Software Version """
 
-    def __init__(self, testing_strategy):
+    def __init__(self, name, testing_strategy):
+        self.name = name
         self.testing_strategy = testing_strategy
         self.reset()
 
     def reset(self):
         """ Clears the testing history """
+
         self.release_reports = []
         self.inflation_reports = []
         self.fix_reports = []
+        self.scores = []
 
     def report(self, issue_list):
         """ Reports a number of defects on the System """
 
-        # TODO(cgavidia) Implement proper reporting logic, based on the Tester
-        # findings and it's learning pattern
         inflation, report = self.testing_strategy.report(issue_list)
         self.record(test_report=report, inflation_report=inflation)
-
         return report
 
-    def record(self, test_report=None, inflation_report=None,fix_report=None):
+    def record(self, test_report=None, inflation_report=None, fix_report=None):
         """ Stores the report made """
 
-        #TODO(cgavidia): I'm not sure if I'm going to use this. Just in case.
         if test_report:
             self.release_reports.append(test_report)
 
@@ -175,8 +183,15 @@ class Tester:
 
         if fix_report:
             self.fix_reports.append(fix_report)
+            self.scores.append(self.get_score(fix_report))
 
-class SoftwareTesting:
+    def get_score(self, fix_report):
+        """ Calculates the payoff function given an specific outcome """
+        return fix_report[SEVERE_KEY] * SEVERE_VALUE + \
+                fix_report[DEFAULT_KEY] * DEFAULT_VALUE + \
+                fix_report[NON_SEVERE_KEY] * NONSEVERE_VALUE
+
+class SoftwareTesting(object):
     """ Manages the testing of an specific release """
 
     def __init__(self, tester_team, developer_team, load_generator,
@@ -186,27 +201,19 @@ class SoftwareTesting:
 
         self.load_generator = load_generator
         self.priority_probabilities = priority_probabilities
-
-        #TODO(cgavidia): I'm not sure if I'm going to use this. Just in case.
-        self.release_reports = []
-        self.fix_reports = []
-
+        self.time_frames = None
 
     def test_and_fix(self, time_frames=4):
         """ Executes the testing simulation for a number of releases. It includes now
         the fix procedure"""
-        #TODO(cgavidia): Evaluate if this is convenient: To first execute ALL
-        # the reporting and then ALL the fixing.
 
         self.time_frames = time_frames
 
-        for time_period in range(time_frames):
+        for _ in range(time_frames):
             test_reports = [tester.report(self.generate_issue_batch())
                             for tester in self.tester_team]
-            self.release_reports.append(test_reports)
 
             fix_reports = self.developer_team.fix(test_reports)
-            self.fix_reports.append(fix_reports)
 
             for index, tester in enumerate(self.tester_team):
                 tester.record(fix_report=fix_reports[index])
@@ -227,7 +234,7 @@ class SoftwareTesting:
         for _ in range(time_frame_issues):
             priority = self.assign_priority()
             issue_batch[priority] += 1
-        
+
         print 'issue_batch ', issue_batch
         return issue_batch
 
@@ -246,27 +253,27 @@ class SoftwareTesting:
     def consolidate_report(self):
         """ Generetes time-frame consolidated information """
 
-        consolidated_reports =[]
+        consolidated_reports = []
 
         for time_frame_index in range(self.time_frames):
-            severe_reported =[tester.release_reports[time_frame_index][SEVERE_KEY]
-                                for tester in self.tester_team]
-            default_reported =[tester.release_reports[time_frame_index][DEFAULT_KEY]
+            severe_reported = [tester.release_reports[time_frame_index][SEVERE_KEY]
+                               for tester in self.tester_team]
+            default_reported = [tester.release_reports[time_frame_index][DEFAULT_KEY]
                                 for tester in self.tester_team]
             nonsevere_reported = [tester.release_reports[time_frame_index][NON_SEVERE_KEY]
-                                for tester in self.tester_team]
+                                  for tester in self.tester_team]
 
             default_inflated = [tester.inflation_reports[time_frame_index][DEFAULT_KEY]
                                 for tester in self.tester_team]
             nonsevere_inflated = [tester.inflation_reports[time_frame_index][NON_SEVERE_KEY]
-                                for tester in self.tester_team]
+                                  for tester in self.tester_team]
 
-            severe_fixed =[tester.fix_reports[time_frame_index][SEVERE_KEY]
-                                for tester in self.tester_team]
-            default_fixed =[tester.fix_reports[time_frame_index][DEFAULT_KEY]
-                                for tester in self.tester_team]
+            severe_fixed = [tester.fix_reports[time_frame_index][SEVERE_KEY]
+                            for tester in self.tester_team]
+            default_fixed = [tester.fix_reports[time_frame_index][DEFAULT_KEY]
+                             for tester in self.tester_team]
             nonsevere_fixed = [tester.fix_reports[time_frame_index][NON_SEVERE_KEY]
-                                for tester in self.tester_team]
+                               for tester in self.tester_team]
 
             consolidated_reports.append({SEVERE_KEY + REPORTED_SUFFIX: sum(severe_reported),
                                          DEFAULT_KEY + REPORTED_SUFFIX: sum(default_reported),
@@ -279,7 +286,35 @@ class SoftwareTesting:
 
         return consolidated_reports
 
-if __name__ == "__main__":
+    def plot_simulation_results(self):
+        """ Plots the result of the simulation, specially the inflation ratio
+        """
+        consolidated_reports = self.consolidate_report()
+        total_issues = [report[SEVERE_KEY + REPORTED_SUFFIX] +
+                        report[DEFAULT_KEY + REPORTED_SUFFIX] +
+                        report[NON_SEVERE_KEY + REPORTED_SUFFIX]
+                        for report in consolidated_reports]
+        inflated_issues = [report[DEFAULT_KEY + INFLATED_SUFFIX] +
+                           report[NON_SEVERE_KEY + INFLATED_SUFFIX]
+                           for report in consolidated_reports]
+        inflation_ratio = [float(inflated)/reported
+                           for inflated, reported in zip(inflated_issues, total_issues)]
+        tester_scores = [sum(tester.scores) for tester in self.tester_team]
+
+        print 'inflation_ratio \n', inflation_ratio
+
+        _, axis = plt.subplots(1, 2, figsize=(15, 5))
+        axis[0].set_title("Inflation Ratio Evolution")
+        axis[0].set_ylabel("Ratio")
+        axis[0].plot(inflation_ratio, linestyle='dashed', marker='o')
+
+        axis[1].set_title("Tester Ranking")
+        axis[1].set_xticklabels(TESTER_NAMES, rotation=90)
+        axis[1].bar(range(len(TESTER_NAMES)), tester_scores)
+
+def main():
+    """ Initial execution point """
+
     print 'Starting simulation ... '
 
     data_frame = data_analysis.load_game_dataset()
@@ -292,17 +327,24 @@ if __name__ == "__main__":
     test_productivity_kernel = stats.gaussian_kde(test_productivity_data)
 
     dev_team = DeveloperTeam(dev_productivity_kernel)
+    tester_team = [Tester(name, StochasticInflationStrategy(strategy))
+                   for name, strategy in zip(TESTER_NAMES, POPULATION_STRATEGIES)]
 
-    rayeesn = Tester(StochasticInflationStrategy(0.06, 0.97))
-    parth = Tester(StochasticInflationStrategy(0.09, 0.56))
-
-    tester_team = [rayeesn, parth]
     priority_probabilities = data_analysis.get_priority_dictionary(data_frame)
     print 'priority_probabilities \n', priority_probabilities
 
     product_testing = SoftwareTesting(tester_team, dev_team, test_productivity_kernel,
                                       priority_probabilities)
     product_testing.test_and_fix(TIME_FRAMES)
-    fixes = product_testing.consolidate_report()
-    print 'fixes ', fixes
+    consolidated_report = product_testing.consolidate_report()
+    print 'consolidated_report ', consolidated_report
+
+    product_testing.plot_simulation_results()
+
+
+if __name__ == "__main__":
+
+    for _ in range(10):
+        main()
+
 
